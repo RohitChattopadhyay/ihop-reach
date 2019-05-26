cf1be0248c7409c179c3b5392907e774fcf3d72f
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2019-05-27 00:22:12 +0530

    Added dataset download in bash file
    Download requires wget, thus wget installed using Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,8 @@ RUN chmod 777 /home/setup.sh
 WORKDIR /home
 # For installing unzip package
 RUN apt-get update
-# Install unzip 
+# Install wget and unzip 
+RUN apt-get install wget
 RUN apt-get install unzip
 # Command to start the image
 CMD /home/setup.sh
--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 mkdir -p /data/db
 touch /var/log/mongodb.log
 chmod 777 /var/log/mongodb.log
-
+echo "Starting MongoDB"
 # Start mongodb with logging
 # --logpath    Without this mongod will output all log information to the standard output.
 # --logappend  Ensure mongod appends new entries to the end of the logfile. We create it first so that the below tail always finds something
@@ -20,16 +20,23 @@ while [[ $? -ne 0 && $COUNTER -lt 60 ]] ; do
     grep -q 'waiting for connections on port' /var/log/mongodb.log
 done
 
+# Download the dataset
+echo "Downloading Dataset"
+wget -cO - https://s3.amazonaws.com/reach.lunean.com/iHOP_Full_Dataset_mongo_import_051919.zip > /home/dataset.zip
 # Unzip source
+echo "Unzip the downloaded Dataset"
 unzip /home/dataset.zip -d /home/unzipDestDataset
 
 # Restore from dump
+echo "Mongo restore initiated"
 mongorestore --drop /home/unzipDestDataset
 
 #delete  source files bash file
+echo "Removing temporary files"
 rm -rf /home/unzipDestDataset
 rm /home/setup.sh
 rm /home/dataset.zip
 
+echo "Image is running"
 # Keep container running
 tail -f /dev/null
