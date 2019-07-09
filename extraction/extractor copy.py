9cc5d643e3c08146c417f4804d2f1be02be1fa3b
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2019-07-10 00:56:48 +0530

    Add PublicationTypeList extraction from XML

---
+++ extraction/extractor copy.py
@@ -0,0 +1,77 @@
+import os,gzip,time,sys,csv
+import indra.literature.pubmed_client as parser
+import xml.etree.ElementTree as ET
+start_time = time.time()
+
+if(len(sys.argv)!=2):
+    print("Less arguments provided")
+    print("python <SCRIPT_NAME>.py <Source Path>")
+    print("Closing Script")
+    quit()
+
+def extractFromXML(fileContent):
+    destFileName = "output.csv"
+    if(os.path.isfile(destFileName)):
+        destCSV = open(destFileName, 'a')
+    else:
+        destCSV = open(destFileName, 'w')
+        print("Journal Title,Year,DOI,PMCID,PMID,Article Type", file=destCSV)
+    writer = csv.writer(destCSV, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    tree = ET.fromstring(fileContent)
+    pm_articles = tree.findall('./PubmedArticle')
+    # TODO change to indra package function get_metadata
+    for art_ix, pm_article in enumerate(pm_articles):
+        medline_citation = pm_article.find('./MedlineCitation')
+        pubmed = pm_article.find('./PubmedData')
+        try:
+            history_pub_date =  pubmed.find('./History/PubMedPubDate[@PubStatus="pubmed"]')
+            year = parser._find_elem_text(history_pub_date, 'Year')
+            month = parser._find_elem_text(history_pub_date, 'Month')
+            day = parser._find_elem_text(history_pub_date, 'Day')
+            pubType = parser._find_elem_text(medline_citation.find('./PublicationTypeList'),'PublicationType')            
+        except expression as err:
+            print(err)
+            continue
+        # Add the Publication date from Journal info
+
+        pub_date = {
+            "year"  : None if (year  is None) else int(year),
+            "month" : None if (month is None) else int(month),
+            "day"   : None if (day   is None) else int(day)
+        }
+        # Get article info
+        article_info = parser._get_article_info(medline_citation, pm_article.find('PubmedData'))
+        # Get journal info
+        journal_info = parser._get_journal_info(medline_citation, False)
+
+        # Preparing results
+        title   = journal_info["journal_abbrev"] or ""
+        Year    = pub_date["year"]
+        DOI     = article_info["doi"] or ""
+        PMCID   = article_info["pmcid"] or ""
+        PMID    = article_info["pmid"] or ""
+        article_type = pubType or ""
+        # storing in csv file 
+        writer.writerow([title,Year,DOI,PMCID,PMID,article_type])
+    # Closing file
+    destCSV.close()
+
+
+rootDir = sys.argv[1] or None
+directory = os.fsencode(rootDir)
+dirlist = os.listdir(directory)
+dirlist.sort()
+print("Total files found: ", len(dirlist))
+for file in dirlist:
+     filename = os.fsdecode(file)
+     if filename.endswith(".gz"): 
+         filePath = os.path.join(rootDir,filename)
+         print("Processing ",filename)
+         with gzip.open(filePath, 'r') as f:
+            fileContent = f.read()
+            extractFromXML(fileContent)
+     else:
+         continue
+
+print("--- %s seconds ---" % (time.time() - start_time))
+quit()
