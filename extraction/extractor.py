e75e2589301d6e40ea96eb883a01544159022a9e
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2019-08-15 12:25:15 +0530

    Add mesh_headings

--- extraction/extractor.py
+++ extraction/extractor.py
@@ -15,23 +15,50 @@ def extractFromXML(fileContent):
         destCSV = open(destFileName, 'a')
     else:
         destCSV = open(destFileName, 'w')
-        print("Journal Title,Year,DOI,PMCID,PMID", file=destCSV)
-    tree = ET.fromstring(fileContent)
-    try:
-        results = parser.get_metadata_from_xml_tree(tree)
-    except Exception as e:
-        print(e)
-        return
-    
-    # preparing for print
+        print("Journal Title,Year,DOI,PMCID,PMID,Article Type,Topics", file=destCSV)
     writer = csv.writer(destCSV, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for key,value in results.items():
-        title   = value["journal_abbrev"]
-        year    = None if value["publication_date"]["year"] is None else value["publication_date"]["year"]
-        doi     = value["doi"]
-        pmcid   = value["pmcid"]
-        pmid    = value["pmid"]
-        writer.writerow([title,year,doi,pmcid,pmid])
+    tree = ET.fromstring(fileContent)
+    pm_articles = tree.findall('./PubmedArticle')
+    # TODO change to indra package function get_metadata
+    for art_ix, pm_article in enumerate(pm_articles):
+        medline_citation = pm_article.find('./MedlineCitation')
+        pubmed = pm_article.find('./PubmedData')        
+        try:
+            history_pub_date =  pubmed.find('./History/PubMedPubDate[@PubStatus="pubmed"]')
+            year = parser._find_elem_text(history_pub_date, 'Year')
+            # month = parser._find_elem_text(history_pub_date, 'Month')
+            # day = parser._find_elem_text(history_pub_date, 'Day')
+            PublicationTypeList = medline_citation.find('./Article/PublicationTypeList')
+            pubType = parser._find_elem_text(PublicationTypeList, 'PublicationType')
+            topics = []
+            for topic in medline_citation.findall('./MeshHeadingList/MeshHeading'):
+                topics.append(topic.find('DescriptorName').text)
+            topics_string = ' , '.join(topics)
+        except Exception as err:
+            print(err)
+            continue
+        # Add the Publication date from Journal info
+
+        pub_date = {
+            "year"  : None if (year  is None) else int(year),
+            # "month" : None if (month is None) else int(month),
+            # "day"   : None if (day   is None) else int(day)
+        }
+        # Get article info
+        article_info = parser._get_article_info(medline_citation, pm_article.find('PubmedData'))
+        # Get journal info
+        journal_info = parser._get_journal_info(medline_citation, False)
+
+        # Preparing results
+        title   = journal_info["journal_abbrev"] or ""
+        Year    = pub_date["year"]
+        DOI     = article_info["doi"] or ""
+        PMCID   = article_info["pmcid"] or ""
+        PMID    = article_info["pmid"] or ""
+        article_type = pubType or ""
+        article_topics = topics_string or ""
+        # storing in csv file 
+        writer.writerow([title,Year,DOI,PMCID,PMID,article_type,article_topics])
     # Closing file
     destCSV.close()
 
