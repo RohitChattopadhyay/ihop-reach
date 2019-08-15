587998b9119b19f6a7325bb18cb7223ae562c474
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2019-08-15 12:26:00 +0530

    Update extractor copy.py

--- extraction/extractor copy.py
+++ extraction/extractor copy.py
@@ -15,29 +15,34 @@ def extractFromXML(fileContent):
         destCSV = open(destFileName, 'a')
     else:
         destCSV = open(destFileName, 'w')
-        print("Journal Title,Year,DOI,PMCID,PMID,Article Type", file=destCSV)
+        print("Journal Title,Year,DOI,PMCID,PMID,Article Type,Topics", file=destCSV)
     writer = csv.writer(destCSV, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     tree = ET.fromstring(fileContent)
     pm_articles = tree.findall('./PubmedArticle')
     # TODO change to indra package function get_metadata
     for art_ix, pm_article in enumerate(pm_articles):
         medline_citation = pm_article.find('./MedlineCitation')
-        pubmed = pm_article.find('./PubmedData')
+        pubmed = pm_article.find('./PubmedData')        
         try:
             history_pub_date =  pubmed.find('./History/PubMedPubDate[@PubStatus="pubmed"]')
             year = parser._find_elem_text(history_pub_date, 'Year')
-            month = parser._find_elem_text(history_pub_date, 'Month')
-            day = parser._find_elem_text(history_pub_date, 'Day')
-            pubType = parser._find_elem_text(medline_citation.find('./PublicationTypeList'),'PublicationType')            
-        except expression as err:
+            # month = parser._find_elem_text(history_pub_date, 'Month')
+            # day = parser._find_elem_text(history_pub_date, 'Day')
+            PublicationTypeList = medline_citation.find('./Article/PublicationTypeList')
+            pubType = parser._find_elem_text(PublicationTypeList, 'PublicationType')
+            topics = []
+            for topic in medline_citation.findall('./MeshHeadingList/MeshHeading'):
+                topics.append(topic.find('DescriptorName').text)
+            topics_string = ' , '.join(topics)
+        except Exception as err:
             print(err)
             continue
         # Add the Publication date from Journal info
 
         pub_date = {
             "year"  : None if (year  is None) else int(year),
-            "month" : None if (month is None) else int(month),
-            "day"   : None if (day   is None) else int(day)
+            # "month" : None if (month is None) else int(month),
+            # "day"   : None if (day   is None) else int(day)
         }
         # Get article info
         article_info = parser._get_article_info(medline_citation, pm_article.find('PubmedData'))
@@ -51,8 +56,9 @@ def extractFromXML(fileContent):
         PMCID   = article_info["pmcid"] or ""
         PMID    = article_info["pmid"] or ""
         article_type = pubType or ""
+        article_topics = topics_string or ""
         # storing in csv file 
-        writer.writerow([title,Year,DOI,PMCID,PMID,article_type])
+        writer.writerow([title,Year,DOI,PMCID,PMID,article_type,article_topics])
     # Closing file
     destCSV.close()
 
